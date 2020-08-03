c1c79c5f4a23a09a755f571c758907918f5c5485
Menphis <menphisj@gmail.com>
2020-08-03 13:01:59 -0400

    maldita s de mierda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl:"https://xionis.envioseurocarga.com/backend/public/index.php",
-  baseApi:"https://xionis.envioseurocarga.com/backend",
+  baseApi:"http://xionis.envioseurocarga.com/backend",
   production: true
   
 };
