dee7fe033ebd5dd30d8dfd3fb1b45c0981c342de
Menphis <menphisj@gmail.com>
2020-08-14 12:49:46 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
-  apiUrl:"https://xionis.es/xionis/backend/public",
-  baseApi:"http://xionis.es/xionis/backend",
+  // apiUrl:"https://xionis.es/xionis/backend/public",
+  // baseApi:"http://xionis.es/xionis/backend",
+  // apiUrl:"http://directorio7.com.mx/backend/public/",
+  // baseApi:"http://directorio7.com.mx/backend",
+  apiUrl:"https://xionis.envioseurocarga.com/backend/public/index.php",
+  baseApi:"http://xionis.envioseurocarga.com/backend",
   production: true
   
 };
